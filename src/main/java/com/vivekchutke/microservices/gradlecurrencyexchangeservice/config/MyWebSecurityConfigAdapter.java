68bee98f42fb3a3707812dbf99418ff65f481a4d
vivekchutke <vivekchutke@gmail.com>
2019-07-25 22:56:10 -0700

    Added loggerator so logs can be sent to papertrail

--- src/main/java/com/vivekchutke/microservices/gradlecurrencyexchangeservice/config/MyWebSecurityConfigAdapter.java
+++ src/main/java/com/vivekchutke/microservices/gradlecurrencyexchangeservice/config/MyWebSecurityConfigAdapter.java
@@ -13,5 +13,9 @@ public class MyWebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+
+        // Added so we can refresh through http://<AppName>/actuator/refresh. Without this is was giving me forbidden exception.
+        // The issue is still not resolved as now I am getting 404 exception
+        http.csrf().disable();
     }
 }
